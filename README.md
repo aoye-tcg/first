593b394e3f771c0fdc19b5c1ae9d287f431af7b9
Nolan <nonodelattre62@gmail.com>
2026-08-09 22:35:47 +0200

    Déplace le bot dans pokemon

--- README.md
+++
@@ -1,4 +0,0 @@
-# GCC Pokémon Deal Bot
-
-V1 : Bot Telegram hébergé gratuitement sur Render.
-La surveillance GCC Marketplace sera ajoutée dans la V2.
--- pokemon/app.py
+++ pokemon/app.py
@@ -32,5 +32,5 @@ def telegram():
     return 'OK'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 10000)) 
     app.run(host='0.0.0.0', port=port)
